19f1e54071d0814c8698a7643796cd02dcd6a73c
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-19 16:13:16 -0300

    Terminamos o exemplo com for

--- ExemploRepeticao.java
+++ ExemploRepeticao.java
@@ -1,23 +1,34 @@
 import java.util.Random;
+
 public class ExemploRepeticao{
     public static void main(String[] args) {
         Random gerador = new Random();
         double nota1, nota2;
-        //repetição controlada por contador
-        int totalAlunos = 1;
-        //içamento
-        //hoist
-        while (totalAlunos <= 5){
-            //[0.0, 1.0)
+        //inicialização (incluindo, talvez, a declaração)
+        //teste de continuidade
+        //incremento
+        for (int totalAlunos = 1; totalAlunos <= 5; totalAlunos++){
             nota1 = gerador.nextDouble() * 10;
             nota2 = gerador.nextDouble() * 10;
-            double resultado = (nota1 + nota2) / 2;
-            System.out.printf("Média: %.2f\n", resultado);
-            ++totalAlunos;
-            //totalAlunos++;
-            //totalAlunos += 1;
-            //totalAlunos = totalAlunos + 1;
+            System.out.println(
+                String.format("Média: %.2f", (nota1 + nota2) / 2)
+            );
         }
+        // //repetição controlada por contador
+        // int totalAlunos = 1;
+        // //içamento
+        // //hoist
+        // while (totalAlunos <= 5){
+        //     //[0.0, 1.0)
+        //     nota1 = gerador.nextDouble() * 10;
+        //     nota2 = gerador.nextDouble() * 10;
+        //     double resultado = (nota1 + nota2) / 2;
+        //     System.out.printf("Média: %.2f\n", resultado);
+        //     ++totalAlunos;
+        //     //totalAlunos++;
+        //     //totalAlunos += 1;
+        //     //totalAlunos = totalAlunos + 1;
+        // }
         System.out.println("Até mais");
     }
 }
